4316bb1a9211bca0e65ff7e9c9205c7cfadd071f
fayas <fayas@Fayass-MacBook-Air.local>
2024-10-12 17:28:15 +0530

    update layout

---
+++ src/Layout.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+const Layout = () => {
+
+  return (
+    <div className="">
+          <Navbar />
+          <Outlet />
+          <Footer />
+    </div>
+  );
+};
+
+export default Layout; 
