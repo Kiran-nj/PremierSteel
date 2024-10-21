2c81ff1f7d98acca4c1f75becca387723f370613
Kiran <kiran.nj.98@gmail.com>
2024-10-21 14:47:08 +0530

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import SteelProductsShowcase from '../components/Products'
+import VideoCarousel from '../components/VideoCaurosel'
 
 function Home() {
   return (
     <>
+    
     <SteelProductsShowcase />
+  
     </>
   )
 }
