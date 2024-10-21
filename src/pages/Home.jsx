1303f3b479550cf68b3334e19481b2dc50bac833
Kiran <kiran.nj.98@gmail.com>
2024-10-21 15:53:58 +0530

    hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import SteelProductsShowcase from '../components/Products'
+import Hero from '../components/Hero'
 
 
 function Home() {
   return (
     <>
-    
+    <Hero />
     <SteelProductsShowcase />
   
     </>
