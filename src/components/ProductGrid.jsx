fd42e93b4edb4922c289653acbc55578be787821
fayasPA <fayasmuthalib@gmail.com>
2024-11-05 17:21:35 +0530

    updated product and blog

--- src/components/ProductGrid.jsx
+++ src/components/ProductGrid.jsx
@@ -5,39 +5,45 @@ import { FaWhatsapp } from 'react-icons/fa';
 const ProjectGrid = () => {
   return (
     <>
-      <div className='h-fit text-selWhite text-xl md:text-5xl pl-2 md:pl-10 py-5 md:py-10 uppercase font-extralight md:font-thin text-start md:text-center bg-black'>
-        <h2 className=""> <span className='text-selRed'>Stocks</span></h2>
-        <h3><span className='text-selRed'>Available</span> in   All <span className='text-selRed'>Sizes</span> and <span className='text-selRed'>Quantity</span></h3>
+      <div className='h-fit text-selWhite  text-xl md:text-5xl pl-2 md:pl-0 py-5 md:py-10 uppercase font-extralight md:font-thin text-start md:text-center bg-black'>
+        <h2 className="">All <span className='text-selRed'>Stocks</span></h2>
+          <span className="text-xs md:text-base ">Our Products are available in all sizes and quantity</span>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-1 md:px-4 pb-3 md:pb-10 bg-black">
+      
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-1 md:px-4 pb-3 md:pb-10 bg-black overflow-visible">
         {productsList.map((project, index) => (
           <div
             key={index}
             className="relative group h-80 overflow-hidden shadow-lg"
-            style={{ backgroundImage: `url(${project.imageSrc})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
+            style={{
+              backgroundImage: `url(${project.imageSrc})`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+            }}
           >
-            {/* WhatsApp Button */}
+
+            {/* Overlay on hover */}
+            <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-20 transition-opacity duration-300"></div>
+
+            {/* Text Content with Semi-Transparent Background */}
+            <div className="absolute inset-x-0 bottom-0 bg-black/60 px-3 pt-3 flex flex-col justify-end gap-1">
+              <h3 className="text-white text-lg font-semibold leading-tight md:group-hover:text-xl transition-all duration-500 text-shadow-black border border-borderColor w-fit px-3">
+                {project.title}
+              </h3>
+              <p className='text-white text-xs md:text-xs '>
+              Available in All Sizes and Quantity
+              </p>
+            </div>
+
+              {/* whatsapp icon */}
             <a
               href={`https://wa.me/+91${companyPhoneNo}?text=I'm%20interested%20in%20the%20product:%20${encodeURIComponent(project.title)}`}
               target="_blank"
               rel="noopener noreferrer"
-              className="absolute top-4 right-4 z-10 p-2 bg-green-500 text-white bg-green/50 rounded-full opacity-80 hover:opacity-100 transition-opacity duration-300"
+              className="absolute bottom-0 right-4 z-10 p-2 bg-green-500 text-green rounded-full opacity-80 hover:opacity-100 transition-opacity duration-300 animate-pulse"
             >
-              <FaWhatsapp size="1.5rem" />
+              <FaWhatsapp className='text-4xl' />
             </a>
-
-            {/* Overlay on hover */}
-            <div className="absolute inset-0 bg-black bg-opacity-40 group-hover:bg-opacity-60 transition-all duration-500"></div>
-
-            {/* Text Content */}
-            <div className="absolute inset-0 flex flex-col justify-end p-6">
-              <h3 className="text-white text-lg font-semibold leading-tight group-hover:text-xl transition-all duration-500 text-shadow-black">
-                {project.title}
-              </h3>
-              <p className="text-white text-sm opacity-80 mt-2 group-hover:opacity-100 transition-all duration-500 text-shadow-black">
-                {project.category}
-              </p>
-            </div>
           </div>
         ))}
       </div>
--- src/components/StatisticsSection .jsx
+++ src/components/StatisticsSection .jsx
@@ -41,8 +41,8 @@ const StatisticsSection = () => {
   ];
 
   return (
-    <div className="timeline md:p-8">
-      <div className="text-borderColor2 py-10 px-5 md:px-20">
+    <div className="timeline md:p-8 bg-selWhite">
+      <div className="text-selRed py-10 px-5 md:px-20">
         <div className="content-text max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-10 text-center">
           {statsData.map((stat, index) => (
             <div
@@ -52,11 +52,11 @@ const StatisticsSection = () => {
             >
               <div className='flex flex-col justify-center border rounded-full md:rounded-xl px-5 py-5 md:px-10 md:py-10 items-center space-y-2'>
                 
-              <div className="text-4xl text-white">{stat.icon}</div>
+              <div className="text-4xl ">{stat.icon}</div>
               <h3 className="text-xs md:text-sm lg:text-base font-extralight">
                 {stat.label}
               </h3>
-              <p className="text-2xl text-white font-medium font-josefin">
+              <p className="text-2xl  font-medium font-josefin">
                 {stat.value}
               </p>
               </div>
