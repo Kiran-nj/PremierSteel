172a659fb4474b353d4be7c3e13a48913479c9f1
fayasPA <fayasmuthalib@gmail.com>
2024-11-07 10:38:49 +0530

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,117 +1,144 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import companyLogo from '../assets/images/company_logo_bg.png';
-import { FaArrowRight, FaInstagram, FaWhatsapp } from 'react-icons/fa';
-import { MdOutlineCall } from 'react-icons/md';
-import { FiMail } from 'react-icons/fi';
+import companyLogo from '../assets/images/company_logo_bg.png'; // Update the path to your image
+import { FaWhatsapp, FaInstagram } from 'react-icons/fa';
 import { AiOutlineFacebook } from 'react-icons/ai';
+import { FiMail } from 'react-icons/fi';
+import { MdOutlineCall } from 'react-icons/md';
 import { companyEmail, companyPhoneNo, productsList } from '../utils/constants';
 
 function Footer({ setIsEnquiryModalOpen }) {
   return (
     <>
-      <div className='bg-red-950 text-white py-5 md:py-10 flex flex-col items-center'>
-        <div className='text-center text-xl md:text-2xl font-thin mb-4'>
-          <h1>Request a Quote Online <span className='font-bold text-white'>24/7</span></h1>
+      {/* Common Quote Section for All Screens */}
+      <div className='bg-red-950 text-white h-auto flex flex-col justify-center items-center py-5 md:py-10'>
+        <div className='space-y-1 md:space-y-5 flex flex-col justify-center items-center'>
+          <div className='text-center text-xl md:text-5xl font-thin mb-1 md:mb-0'>
+            <h1>Request a Quote Online <span className='font-bold text-white'>24/7</span></h1>
+          </div>
+          <button className="request-button" onClick={() => setIsEnquiryModalOpen(true)}>
+            <span className="text">Request a Quote</span>
+          </button>
         </div>
-
-        <button
-          className="request-button flex items-center gap-2 px-4 py-2 bg-white text-black rounded-full hover:bg-gray-200 transition-all"
-          onClick={() => setIsEnquiryModalOpen(true)}
-        >
-          Request a Quote
-          <FaArrowRight />
-        </button>
-
-        <div
-          className='mt-3 text-xs md:text-sm cursor-pointer'
-          onClick={() => window.location.href = `tel:+91${companyPhoneNo}`}
-        >
-          or call +91 {companyPhoneNo}
+        <div className='mt-2 text-xs md:text-base text-white' onClick={() => window.location.href = `tel:+91${companyPhoneNo}`}>
+          <h1>or call +91 {companyPhoneNo}</h1>
         </div>
       </div>
 
-      <div className='bg-black text-[#a9a9a9] px-5 md:px-20 py-8 md:py-14'>
-        <div className="flex flex-col md:flex-row md:justify-between items-center md:items-start">
-          {/* Logo and Company Name */}
-          <div className="flex flex-col items-center mb-4 md:mb-0 md:mr-8 text-white">
-            <NavLink to="/" className="flex items-center gap-2">
-              <img src={companyLogo} alt="Company Logo" className="w-10 h-10" />
-              <h1 className="text-2xl md:text-3xl font-bold">Premier Steels</h1>
-            </NavLink>
+      {/* Footer Section for Larger Screens */}
+      <div className="hidden md:block bg-black md:px-20 py-5 md:py-14">
+        <div className="flex flex-col md:items-center md:mb-4 lg:mb-0 lg:mr-8 text-[#a9a9a9]">
+          <NavLink to="/" className="flex items-center">
+            <img src={companyLogo} alt="Company Logo" className="w-5 h-5 lg:w-10 lg:h-6" />
+            <h1 className="company-font-only text-2xl md:text-3xl font-semibold text-white">Premier Steels</h1>
+          </NavLink>
+          <p className="text-xs md:text-sm text-white hidden md:block">Quality • Strength • Durability</p>
+        </div>
+
+        {/* Location, Products, Information, and Contact Sections */}
+        <div className="flex flex-col lg:flex-row lg:justify-around text-[#a9a9a9] md:space-y-1 md:mt-8">
+          <div className="flex flex-col items-start text-center lg:text-left">
+            <h4 className="font-bold text-sm mb-2 hidden md:block">LOCATION</h4>
+            <p className="text-xs">STOCK YARD, PULLEPADY ROAD</p>
+            <p className="text-xs">ERNAKULAM, PIN: 628017</p>
           </div>
-
-          {/* Location, Products, Information, Contact Sections */}
-          <div className="flex flex-col md:flex-row gap-10 md:gap-20 text-sm w-full md:w-auto">
-            <div className="text-center md:text-left">
-              <h4 className="font-bold mb-2">LOCATION</h4>
-              <p>STOCK YARD, PULLEPADY ROAD</p>
-              <p>ERNAKULAM, PIN: 628017</p>
-            </div>
-            <div className='flex gap-20 mx-auto'>
-            <div className="text-center md:text-left">
-              <h4 className="font-bold mb-2">PRODUCTS</h4>
-              <ul className="space-y-1">
-                {productsList.slice(0, 5).map((prod, index) => (
-                  <li key={index} className="uppercase">{prod.title}</li>
-                ))}
-              </ul>
-            </div>
-
-            <div className="text-center md:text-left">
-              <h4 className="font-bold mb-2">INFORMATION</h4>
-              <ul className="space-y-1">
-                <li>HOME</li>
-                <li>PRODUCTS</li>
-                <li>BLOG</li>
-                <li>ABOUT</li>
-                <li>CONTACT</li>
-              </ul>
-            </div>
-            </div>
-
-            <div className="text-center md:text-left justify-center">
-  <h4 className="font-bold mb-2">CONTACT US</h4>
-  <p
-    className="flex items-center gap-2 cursor-pointer justify-center"
-    onClick={() => window.location.href = `mailto:${companyEmail}`}
-  >
-    <FiMail /> {companyEmail}
-  </p>
-  <p
-    className="flex items-center gap-2 cursor-pointer mt-1 justify-center"
-    onClick={() => window.location.href = `tel:+91${companyPhoneNo}`}
-  >
-    <MdOutlineCall /> +91 {companyPhoneNo}
-  </p>
-</div>
-
+          <div className="flex flex-col items-start text-center lg:text-left">
+            <h4 className="font-bold text-sm mb-2">PRODUCTS</h4>
+            <ul className="space-y-1 text-xs">
+              {productsList.slice(0, 5).map((prod, index) => (
+                <li key={index} className="uppercase">{prod.title}</li>
+              ))}
+            </ul>
+          </div>
+          <div className="flex flex-col items-start text-center lg:text-left">
+            <h4 className="font-bold text-sm mb-2">INFORMATION</h4>
+            <ul className="space-y-1 text-xs">
+              <li>HOME</li>
+              <li>PRODUCTS</li>
+              <li>BLOG</li>
+              <li>ABOUT</li>
+              <li>CONTACT</li>
+            </ul>
+          </div>
+          <div className="flex flex-col items-start text-center lg:text-left">
+            <h4 className="font-bold text-sm mb-2 hidden md:block">CONTACT US</h4>
+            <span className="cursor-pointer text-xs flex items-center gap-1" onClick={() => window.location.href = `mailto:${companyEmail}`}>
+              <FiMail className="text-xl" /> {companyEmail}
+            </span>
+            <span className="cursor-pointer text-xs flex items-center gap-1" onClick={() => window.location.href = `tel:+91${companyPhoneNo}`}>
+              <MdOutlineCall className='text-xl' /> +91 {companyPhoneNo}
+            </span>
           </div>
         </div>
 
-        {/* Social Icons */}
-        <div className="mt-8 flex justify-center gap-5">
+        <div className="flex justify-center items-center gap-4 pt-12 text-white">
           <a href={`https://wa.me/+91${companyPhoneNo}`} target="_blank" rel="noreferrer">
-            <FaWhatsapp className="text-white hover:text-gray-400 transition-colors" size="1.5rem" />
+            <FaWhatsapp size="1.5rem" />
           </a>
           <a href="https://www.facebook.com/people/Premier-Steels/100070995211277/" target="_blank" rel="noreferrer">
-            <AiOutlineFacebook className="text-white hover:text-gray-400 transition-colors" size="1.5rem" />
+            <AiOutlineFacebook size="1.5rem" />
           </a>
           <a href="https://www.instagram.com/premiersteelsindia/" target="_blank" rel="noreferrer">
-            <FaInstagram className="text-white hover:text-gray-400 transition-colors" size="1.5rem" />
+            <FaInstagram size="1.5rem" />
           </a>
         </div>
       </div>
 
-      {/* Footer Bottom */}
-      <footer className="text-[#a9a9a9] py-2 bg-black text-center text-xs">
-        <p>©2024 Premier Steels | All Rights Reserved.</p>
-        <p className="mt-1">
-          Powered by
-          <a href="https://cyfletech.com" className="text-blue ml-1" target="_blank" rel="noopener noreferrer">
-            <strong className="text-white">Cyfletech</strong>
+      {/* Footer Section for Small Screens */}
+      <div className="block md:hidden bg-black text-[#a9a9a9] py-5">
+        <div className="text-center text-white mb-4">
+          <h1 className="text-xl font-semibold">Premier Steels</h1>
+          <p className="text-xs">Quality • Strength • Durability</p>
+        </div>
+
+        <div className="text-center space-y-2 text-xs">
+          <h4 className="font-bold">LOCATION</h4>
+          <p>STOCK YARD, PULLEPADY ROAD</p>
+          <p>ERNAKULAM, PIN: 628017</p>
+
+          <div className="flex text-center justify-center items-center gap-20">
+
+            <div>
+          <h4 className="font-bold text-sm mb-2">PRODUCTS</h4>
+          <ul className="space-y-1">
+            {productsList.slice(0, 5).map((prod, index) => (
+              <li key={index} className="uppercase">{prod.title}</li>
+            ))}
+          </ul>
+          </div>
+
+          <div>
+            <h4 className="font-bold text-sm mb-2">INFORMATION</h4>
+            <ul className="space-y-1 text-xs">
+              <li>HOME</li>
+              <li>PRODUCTS</li>
+              <li>BLOG</li>
+              <li>ABOUT</li>
+              <li>CONTACT</li>
+            </ul>
+            </div>
+          </div>
+
+          <h4 className="font-bold mt-3">CONTACT</h4>
+          <span onClick={() => window.location.href = `mailto:${companyEmail}`} className="cursor-pointer block">Email: {companyEmail}</span>
+          <span onClick={() => window.location.href = `tel:+91${companyPhoneNo}`} className="cursor-pointer block">Phone: +91 {companyPhoneNo}</span>
+        </div>
+
+        <div className="flex justify-center gap-4 pt-4 text-white">
+          <a href={`https://wa.me/+91${companyPhoneNo}`} target="_blank" rel="noreferrer">
+            <FaWhatsapp size="1.5rem" />
           </a>
-        </p>
+          <a href="https://www.facebook.com/people/Premier-Steels/100070995211277/" target="_blank" rel="noreferrer">
+            <AiOutlineFacebook size="1.5rem" />
+          </a>
+          <a href="https://www.instagram.com/premiersteelsindia/" target="_blank" rel="noreferrer">
+            <FaInstagram size="1.5rem" />
+          </a>
+        </div>
+      </div>
+
+      <footer className="text-lineGray py-1 bg-black text-center text-[.5rem]">
+        <p>©2024 Premier Steels | All Rights Reserved. Powered by <a href="https://cyfletech.com" target="_blank" rel="noopener noreferrer" className="text-blue">Cyfletech</a></p>
       </footer>
     </>
   );
